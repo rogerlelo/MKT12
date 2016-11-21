7404288a622b56131dd26bcdb1c11b5ad6a8a210
Rogerio de Souza Antonio <rogeriodesouzaantonio@ig.com.br>
2016-11-21 12:54:00 -0200

    problema rota tags inexistente e views

---
+++ src/CodeEmailMKT/Application/Action/Tag/Factory/TagListPageFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CodeEmailMKT\Application\Action\Tag\Factory;
+
+use Interop\Container\ContainerInterface;
+use Zend\Expressive\Template\TemplateRendererInterface;
+use CodeEmailMKT\Domain\Persistence\CustomerRepositoryInterface;
+//use CodeEmailMKT\Application\Action\Customer\CustomerListPageAction;
+use CodeEmailMKT\Application\Action\Tag\TagListPageAction;
+
+
+class TagListPageFactory
+{
+    public function __invoke(ContainerInterface $container) :TagListPageAction
+    {
+        $template = $container->get(TemplateRendererInterface::class);
+        $repository = $container->get(CustomerRepositoryInterface::class);
+        return new TagListPageAction($repository,$template);
+    }
+}
